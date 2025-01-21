cb4bff24cef7f831586b4907c021be07b225c1dc
leiliheidarishizari <leiliheidarishizari@gmail.com>
2025-01-21 18:25:36 +0000

    Deleting show-card class

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function displayShowList(shows) {
   rootElem.innerHTML = "";
   shows.forEach((show) => {
     const showCard = document.createElement("div");
-    showCard.className = "show-card";
+  
 
     const showImage = document.createElement("img");
     showImage.src = show.image ? show.image.medium : "placeholder.jpg";
